b152832a164cd422993b143bdce897cab6f60008
Fehmina <fehmina.hasan@gmail.com>
2024-06-30 13:12:08 -0700

    fix resume style and animation

--- personal-portfolio/src/components/Resume.js
+++ personal-portfolio/src/components/Resume.js
@@ -1,47 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from '@react-spring/web';
-import { Viewer, Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import { pdfjs } from 'react-pdf';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import '../styles/Resume.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Resume = () => {
   const props = useSpring({ opacity: 1, from: { opacity: 0 }, delay: 200 });
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   const [showResume, setShowResume] = useState(false);
   const [buttonText, setButtonText] = useState("View Resume")
 
-const toggleResume = () => {
-  setShowResume(!showResume);
-  if(buttonText === "View Resume") {
-    setButtonText("Hide Resume")
-  }
-  else {
-    setButtonText("View Resume")
-  }
-};
+  const toggleResume = () => {
+    setShowResume(!showResume);
+    if (buttonText === "View Resume") {
+      setButtonText("Hide Resume")
+    }
+    else {
+      setButtonText("View Resume")
+    }
+  };
+
+  const slideInAnimation = useSpring({
+    height: showResume ? '65rem' : '0px',
+    opacity: showResume ? 1 : 0,
+    overflow: 'hidden',
+    config: { tension: 200, friction: 30 }
+  });
+
   return (
     <animated.section id="resume" className="resume-section" style={props}>
       <div className="container">
         <h2 className="display-4 mb-4">Resume</h2>
         <p className='text-center'>
           <button
-          onClick={toggleResume}
+            onClick={toggleResume}
             className="btn btn-primary mb-4"
           >
             {buttonText}
           </button>
-          {showResume && (
-           <div className="pdf-viewer">
-            <Worker workerUrl={`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`}>
-              <Viewer fileUrl="/DE_Resume.pdf" plugins={[defaultLayoutPluginInstance]} />
-            </Worker>
-          </div>
-          )}
         </p>
+        <animated.div style={slideInAnimation} className="pdfResume">
+          <iframe title='view resume' src="/DE_Resume.pdf#zoom=90" width="100%" height="1025rem" />
+        </animated.div>
       </div>
     </animated.section>
   );
